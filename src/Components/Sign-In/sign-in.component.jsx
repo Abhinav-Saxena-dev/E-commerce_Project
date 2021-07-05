4510feb449f24fb63d169b90e0b8a8289e60d40f
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2021-07-05 15:23:08 +0530

    Completed Sign-up and Sign-in page with full functionality

--- src/Components/Sign-In/sign-in.component.jsx
+++ src/Components/Sign-In/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.style.scss';
 import FormInput from '../Form-input/form-input.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 import CustomButton from '../custom-button/custom-buttom.component';
 
 class SignIn extends React.Component{
@@ -12,9 +12,18 @@ constructor(props){
         password : "",
     };
 }
-handleSubmit = (event) => {
+handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email : "" , password : ""});
+
+    const { email, password } = this.state;
+
+    try{
+        await auth.signInWithEmailAndPassword(email, password);
+        this.setState({ email : "" , password : ""});
+    }catch(error){
+        console.log(error);
+    }
+
 }
 
 handleChange = (event) => {
